ba13237cf20a5b1278affc57cfc5789dc752317e
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-26 04:50:19 -0700

    Looping and Iteration JS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+const occasion = 'surprise'
+
+function writeCards(names, occasion) {
+    const array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    } return array
+}
+
+function countdown(num){
+    while (num >= 0){
+        console.log( num--)
+    }
+}
